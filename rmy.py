34b81b7573dde1a7d57f59a292f45bc355eb14b4
paxarsenica <aclak97@gmail.com>
2021-04-28 00:57:59 -0400

    feat: add loot, roth, about, and bot commands

--- rmy.py
+++ rmy.py
@@ -11,22 +11,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from apscheduler.schedulers.background import BackgroundScheduler
-import time
 
 client = discord.Client()
 
 # Loads env file with Discord Token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+PERSONAL_FINANCE = os.getenv('PERSONAL_FINANCE_CHANNEL_ID')
 bot_prefix = '!rmy'
+
+commands_dict = {
+    'about': 'A brief description of the purpose and functionality of this Discord bot.',
+    'commands': 'I wonder what this does... :thinking:',
+    'pubsub': 'What\'s on the menu this week? This command retrieves Publix\'s sub sandwich that is on sale this week.',
+    'flaccid': 'Wanna let the world know you love flaccid fries? Use this command!',
+    'Others:': 'loot, roth, bot'
+}
 
 opts = ChromeOptions()
 opts.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 opts.add_argument("--headless")
 opts.add_argument("--no-sandbox")
 opts.add_argument("--disable-dev-sh-usage")
-
 driver = Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=opts)
 
 scheduler = BackgroundScheduler()
@@ -48,21 +54,35 @@ async def on_message(message):
     print(args)
 
     if args [0] == 'about':
-        return
-    
-    if args [0] == 'help':
-        return
-
-    # Command Level
-    if args[0] == 'pubsub':
+        rmy_bot_logo = discord.File("./img/rmybot.png", filename="rmybot.png")
+        about_embed = discord.Embed(title='RMY Bot', color=discord.Color.red(), description='My name is RMY Bot v0.1. My purpose is to entertain the members of RMY with fun and useful commands. Type `!rmy commands` to learn more about what I can do.')
+        about_embed.set_image(url='attachment://rmybot.png')
+        await message.channel.send(file=rmy_bot_logo, embed=about_embed)
+    elif args [0] == 'commands':
+        command_embed = discord.Embed(title='RMY Bot Commands', color=discord.Color.red(), description='Here\'s a list of my commands:')
+        for command in commands_dict:
+            command_embed.add_field(name=command, value=commands_dict[command], inline=False)
+        await message.channel.send(embed=command_embed)
+    # Commands
+    elif args[0] == 'pubsub':
         sub_response = get_sub_of_the_week() if (len(args) == 2 and args[1] == 'update') else get_sub()
         if sub_response != '':           
             await message.channel.send(f'This week\'s Publix Sub is the {sub_response}!')
+            if sub_response.find('Tender') != -1:
+                await message.channel.send('https://tenor.com/view/lets-go-lets-goo-lest-gooooooooooooooooo-gif-19416648')
         else:
             await message.channel.send('There was an error while retrieving the Sub of the Week. Please try again later.')
-
-    if args[0] == 'flaccid':
+    elif args[0] == 'roth':
+        await message.channel.send(f'You telling me you don\'t have a Roth IRA? You better open one up right mf now! And while you\'re at it, check out my guide to personal finance (see the pinned post in the <#{PERSONAL_FINANCE}> channel)!')
+    elif args[0] == 'loot':
+        await message.channel.send('It wouldn\'t be me if I didn\'t loot.')
+    elif args[0] == 'bot':
+        await message.channel.send('https://thumbs.gfycat.com/BonyLonelyDesertpupfish-size_restricted.gif')
+    elif args[0] == 'flaccid':
         await message.channel.send('https://tenor.com/view/meryl-streep-flacid-gif-10066576')
+    else:
+        await message.channel.send('I don\'t recognize that command. Please type `!rmy commands` to see a list of what I can do!')
+
 
 def get_sub_of_the_week():
     global sotw
@@ -103,7 +123,6 @@ def set_publix_store():
 
 @scheduler.scheduled_job('cron', day_of_week='wed', hour=11)
 def get_sub_of_the_week_scheduler():
-    print('getting subs')
     get_sub_of_the_week()
 
 scheduler.start()
