1c315fb045eede1005bf995ced4eff064b00a613
paxarsenica <aclak97@gmail.com>
2021-07-17 00:46:46 -0400

    feat: add cod.py
    Add basic Call of Duty stat tracking function

--- pubsub.py
+++ pubsub.py
@@ -27,9 +27,9 @@ def get_sub_of_the_week():
     while i < 3:
         print('Fetching Sub of the Week...')
         try:
-            set_publix_store()
-            driver.find_element_by_xpath("//a[@id='deli']").click()
-            sotw = driver.find_element_by_xpath("//div[contains(text(),'Whole')][contains(text(),'Sub')]").text
+            driver.get('https://www.iheartpublix.com/category/weekly-ad/publix-ad-weekly-ad/')
+            driver.find_element_by_xpath("//a[@class='more-link']").click()
+            sotw = driver.find_element_by_xpath("//span[contains(text(),'Whole')][contains(text(),'Sub')]").text
             print('Sub successfully retrieved.')
             break
         except NoSuchElementException as e:
@@ -44,18 +44,3 @@ def get_sub():
         return sotw
     else:
         return get_sub_of_the_week()
-
-def set_publix_store():
-    driver.get('https://www.publix.com')
-    try:
-        driver.find_element_by_xpath("//div[contains(@class, 'store-search-toggle')]//button").click()
-        driver.find_element_by_xpath("//input[@name='ZIP or City, State or store number']").send_keys("30301")
-        driver.find_element_by_xpath("//input[@name='ZIP or City, State or store number']").send_keys(Keys.ENTER)
-        driver.implicitly_wait(4)
-        driver.find_element_by_xpath("//button[contains(text(),'Choose Store')]").click()
-        driver.get('https://www.publix.com/savings/weekly-ad')
-        driver.implicitly_wait(4)
-        print('Store selected.')
-    except NoSuchElementException as e:
-        print(e)
-        print('Store already selected.')
--- rmy.py
+++ rmy.py
@@ -7,6 +7,7 @@ from discord import Embed, Color
 from aiohttp import ClientSession
 from apscheduler.schedulers.background import BackgroundScheduler
 from pubsub import get_sub, get_sub_of_the_week
+from cod import get_cod_stats
 
 client = discord.Client()
 
@@ -15,6 +16,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PERSONAL_FINANCE = os.getenv('PERSONAL_FINANCE_CHANNEL_ID')
 bot_prefix = '!rmy'
+username='Leekpo%231170'
+platform='battle'
 
 commands_dict = {
     'about': 'A brief description of the purpose and functionality of this Discord bot.',
@@ -24,11 +27,14 @@ commands_dict = {
     'Others:': 'loot, roth, bot'
 }
 
+cod_platforms = ['battle', 'acti', 'psn', 'xbl']
+
 scheduler = BackgroundScheduler()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    # await get_cod_stats(username, platform)
 
 @client.event
 async def on_message(message):
@@ -59,6 +65,8 @@ async def on_message(message):
         await message.channel.send('https://thumbs.gfycat.com/BonyLonelyDesertpupfish-size_restricted.gif')
     elif args[0] == 'flaccid':
         await message.channel.send('https://tenor.com/view/meryl-streep-flacid-gif-10066576')
+    elif args[0] == 'balagadoo':
+        await message.channel.send('https://www.youtube.com/watch?v=y7Phst7PoN0')
     
     # Commands
     elif args[0] == 'pubsub':
@@ -69,6 +77,13 @@ async def on_message(message):
                 await message.channel.send('https://tenor.com/view/lets-go-lets-goo-lest-gooooooooooooooooo-gif-19416648')
         else:
             await message.channel.send('There was an error while retrieving the Sub of the Week. Please try again later.')
+    # elif args[0] == 'cod':
+    #     cod_stats(message, args[1:])
+        # TODO
+        # !cod [platform] [username]
+        # Error handling for username not found
+        # Error handling for unauthorized usernames
+        # Embed message 
     # Command not Found
     else:
         await message.channel.send('I don\'t recognize that command. Please type `!rmy commands` to see a list of what I can do!')
@@ -76,6 +91,29 @@ async def on_message(message):
 @scheduler.scheduled_job('cron', day_of_week='wed', hour=11)
 def get_sub_of_the_week_scheduler():
     get_sub_of_the_week()
+
+async def cod_stats(message, params):
+    if len(params) < 2:
+        await message.channel.send('Please include username and platform. Ex. `!cod battle Leekpo#1170`')
+        return
+    if params[0] == 'help':
+        await message.channel.send('help message')
+    elif params[0] == 'platforms':
+        await message.channel.send('These are the platforms.')
+    elif params[0] in cod_platforms:
+        if params[0] == 'activision' or params[0] == 'battle':
+            pc_username = params[1].split('#')
+            if len(pc_username) == 2:
+                params[1] = '%23'.join(pc_username)
+            else:
+                await message.channel.send('Please enter a valid Activision or Battle.net username. Ex. `Leekpo#1170`')
+                return
+        await get_cod_stats(params[1], params[0])
+    else:
+        await message.channel.send('Please enter a valid Call of Duty platform. Type `!cod platforms` for a list of accepted platforms.')
+
+    
+
 
 scheduler.start()
 client.run(TOKEN)
